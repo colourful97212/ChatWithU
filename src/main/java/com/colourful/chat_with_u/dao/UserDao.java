9dad7daa6acf59a6507dee26517458cbc7c61191
Gon <colourful97212@gmail.com>
2019-10-16 18:27:07 +0800

    First Git Project

--- src/main/java/com/colourful/chat_with_u/dao/UserDao.java
+++ src/main/java/com/colourful/chat_with_u/dao/UserDao.java
@@ -6,5 +6,18 @@ import org.apache.ibatis.annotations.Param;
 
 public interface UserDao
 {
+    /**
+     * 根据用户名找一个User对象
+     * @param username 用户名
+     * @return user
+     */
     User findByUsername(@Param("username") String username);
+
+    /**
+     * 添加一个用户
+     * @param username 用户名
+     * @param password 密码
+     * @return 受影响的行数
+     */
+    Integer addUser(@Param("username") String username,@Param("password") String password);
 }
