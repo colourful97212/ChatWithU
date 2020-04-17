b2e4a71e6cd57a41118bb2230888224f0201b28d
gonglige <gonglige@dsgdata.com>
2020-04-17 14:40:28 -0700

    gg

--- src/main/java/com/colourful/chat_with_u/ChatWithUApplication.java
+++ src/main/java/com/colourful/chat_with_u/ChatWithUApplication.java
@@ -2,7 +2,9 @@ package com.colourful.chat_with_u;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
@@ -10,6 +12,7 @@ import org.springframework.cache.annotation.EnableCaching;
 @MapperScan("com.colourful.chat_with_u.dao")
 @SpringBootApplication
 @EnableCaching
+@EnableAutoConfiguration(exclude = {SecurityAutoConfiguration.class})
 public class ChatWithUApplication extends SpringBootServletInitializer
 {
 
